110a88394b27e337e60b0aa0e384e49efa057990
SaarD00ishuman <97211377+SaarD00ishuman@users.noreply.github.com>
2022-08-21 14:06:36 +0530

    wqzewz

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import React from "react";
 import { AiOutlineCalendar } from "react-icons/ai";
 import { IoIosPeople } from "react-icons/io";
@@ -6,10 +7,14 @@ import { IoNotificationsOutline } from "react-icons/io5";
 import Hdropdown from "./Hdropdown";
 const Header = () => {
   const { data: session } = useSession();
+  const router = useRouter();
   return (
     <div className="px-7 pt-3 flex justify-between z-50  bg-gray-100 sticky top-0">
       <div className="flex gap-5">
-        <AiOutlineCalendar className="h-7 w-7 text-gray-500" />
+        <AiOutlineCalendar
+          onClick={() => router.push("/create/post")}
+          className="h-7 w-7 text-gray-500"
+        />
         <IoIosPeople className="h-7 w-7 text-gray-500" />
       </div>
       {/* seperation */}
